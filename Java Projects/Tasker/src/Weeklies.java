9cac7346fc62af3aa3a83670cb90a6521ade1400
wmenkus <77371324+wmenkus@users.noreply.github.com>
2022-01-02 01:58:06 -0500

    apparently there were changes

--- Java Projects/Tasker/src/Weeklies.java
+++ Java Projects/Tasker/src/Weeklies.java
@@ -5,6 +5,32 @@ public class Weeklies {
     private LinkedList<Task> tasks;
     private LinkedList<Task> singleTasks;
     private Random rand;
+    private int taskNum;
 
-    
+    public Weeklies() {
+        tasks = new LinkedList<Task>();
+        singleTasks = new LinkedList<Task>();
+        rand = new Random();
+        taskNum = 0;
+    }
+
+    public void addTask(Task task) {
+        if(task.getTaskValue() == 1) {
+            singleTasks.add(task);
+        }
+        tasks.add(task);
+    }
+
+    public Task getTask() {
+        if(taskNum == 0) {
+            return singleTasks.remove(rand.nextInt(singleTasks.size()));
+        }
+        else {
+            return tasks.remove(rand.nextInt(tasks.size()));
+        }
+    }
+
+    public int getTaskNum() {
+        return taskNum;
+    }
 }
